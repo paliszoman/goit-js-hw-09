afdd08baa97259ac7b585532d27ee4ec4982df86
paliszoman <kamilkrolik@hotmail.com>
2022-08-11 22:22:14 +0200

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body')
+const startButton = document.querySelector('[data-start]')
+const stopButton = document.querySelector('[data-stop]')
+let title = document.querySelector('.title')
+let timerId = null;
 
+let getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let copyToClipboard = () => {
+    let copyText = body.style.backgroundColor;
+    navigator.clipboard.writeText(copyText).then(() => {
+    
+        alert("Color copied to clipboard");
+    })
+}
+
+startButton.addEventListener('click', () => {
+    return timerId = setInterval(
+        () => {return body.style.backgroundColor = getRandomHexColor(),
+        title.innerHTML = `Color of this site is: ${body.style.backgroundColor}`,
+        startButton.disabled = true;}, 1000)
+ })
+
+stopButton.addEventListener('click', () => {
+    return clearInterval(timerId),
+        startButton.disabled = false,
+        copyToClipboard() 
+    })
